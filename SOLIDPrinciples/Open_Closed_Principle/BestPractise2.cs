ee8b90faaa8e97fbdf559b7f062eef7369855f18
Fırat Alçın <firatalcin04@gmail.com>
2023-04-13 01:20:35 +0300

    Open-Closed Principle

---
+++ SOLIDPrinciples/Open_Closed_Principle/BestPractise2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle
+{
+   
+
+    public class LowSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 2;
+        }
+    }
+
+    public class MiddleSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 4;
+        }
+    }
+
+    public class HighSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 6;
+        }
+    }
+
+
+    public class BestPractise2
+    {
+        public decimal Calculate(decimal salary, Func<decimal,decimal> calculateDelege)
+        {
+            return calculateDelege(salary);
+        }
+    }
+}
---
+++ SOLIDPrinciples/Open_Closed_Principle/Program.cs
@@ -0,0 +1,7 @@
+﻿using Open_Closed_Principle;
+
+//BestPractise bestPractise = new BestPractise();
+//Console.WriteLine("Salary : " + bestPractise.Calculate(7000, new MiddleSalaryCalculate()));
+
+BestPractise2 bestPractise2 = new BestPractise2();
+Console.WriteLine("Salary: " + bestPractise2.Calculate(8500,new MiddleSalaryCalculate().Calculate));
