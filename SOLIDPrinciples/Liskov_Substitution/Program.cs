fda174adcf5f7d755a5178805eb581abb1d149e1
Fırat Alçın <firatalcin04@gmail.com>
2023-04-27 00:03:28 +0300

    Liskov Substitution Principle

---
+++ SOLIDPrinciples/Liskov_Substitution/Program.cs
@@ -0,0 +1,38 @@
+﻿public interface ITakePhoto
+{
+    void TakePhoto();
+}
+
+public abstract class BasePhone
+{
+    public void Call()
+    {
+        Console.WriteLine("Arama yapıldı");
+    }
+}
+
+public class IPhone : BasePhone, ITakePhoto
+{
+    public void TakePhoto()
+    {
+        Console.WriteLine("Fotoğraf Çekildi");
+    }
+}
+
+public class Nokia3310 : BasePhone
+{
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IPhone phone1 = new IPhone();
+        phone1.Call();
+        phone1.TakePhoto();
+        Nokia3310 phone2 = new Nokia3310();
+        phone2.Call();
+        
+    }
+}
