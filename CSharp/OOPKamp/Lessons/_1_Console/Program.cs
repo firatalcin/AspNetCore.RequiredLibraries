5e500972650b7eab5ce16125047741a2df6d7137
Fırat Alçın <firatalcin04@gmail.com>
2023-06-20 23:19:01 +0300

    ..

--- CSharp/OOPKamp/Lessons/_1_Console/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("Hello");
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
-
-Console.WriteLine("İsim Giriniz: ");
-string name = Console.ReadLine();
-Console.WriteLine(name);
